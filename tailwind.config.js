0ce0d06cb239d8d41a4a145bae88240350711835
Ibrahim Alassaf <xibra9141@gmail.com>
2022-09-26 20:47:14 +0300

    feat: create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#222222",
+      secondary: "white",
+      grayBorder: "rgb(75,85,99)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
